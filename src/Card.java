fdb35079024ecb2ca185e90e941603ba5888a75d
Shakira <shakira.mccubbin7@gmail.com>
2026-05-07 11:44:33 -0500

    update card number to int
    Add methods following uml diagram: setCardNumber, getCardNumber, setBalance, getBalance, setPin, getPin.
    
    Add methods to Card class to set and get card status
    
    Update Masking report method

--- src/Card.java
+++ src/Card.java
@@ -1,17 +1,42 @@
 public class Card {
     // Attributes were taken from the diagram
-    private String cardNumber;
+    private int cardNumber;
     private double balance;
     private int pin;
     private boolean status;
 
 
-    public Card(String cardNumber, double balance, int pin, boolean status) {
+    public Card(int cardNumber, double balance, int pin, boolean status) {
         this.cardNumber = cardNumber;
         this.balance = balance;
         this.pin = pin;
         this.status = status;
     }
+
+    public void setCardNumber(int _cardNumber) {
+        this.cardNumber = _cardNumber;
+    }
+
+    public int getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setPin(int pin) {
+        this.pin = pin;
+    }
+
+    public int getPin() {
+        return pin;
+    }
+
 
     // Task A: Security Logic
     public void updatePin(int oldPin, int newPin) {
@@ -23,6 +48,14 @@ public class Card {
         }
     }
 
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public boolean getStatus() {
+        return status;
+    }
+
     // Task A: Behavioral Method
     public void activateCard() {
         this.status = true;
@@ -30,9 +63,13 @@ public class Card {
     }
 
     // Task A & Bonus: Reporting with Masking
-//    @Override
-//    public String toString() {
-//        String masked = "XXXX-XXXX-XXXX-" + cardNumber.substring(cardNumber.length() - 4);
-//        return "Card Details: [Number: " + masked + ", Balance: $" + balance + ", Active: " + status + "]";
-//    }
+    @Override
+    public String toString() {
+        return "Card{" +
+                "cardNumber='" + cardNumber + '\'' +
+                ", balance=" + balance +
+                ", pin=****" +
+                ", status=" + status +
+                '}';
+    }
 }
